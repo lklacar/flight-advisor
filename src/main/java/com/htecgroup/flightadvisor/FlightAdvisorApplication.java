eceb38effa85db205096896b9b2b53586dbf62d3
Luka Klacar <lukaklacar@gmail.com>
2020-05-26 15:26:57 +0200

    Removed LiquibaseProperties

--- src/main/java/com/htecgroup/flightadvisor/FlightAdvisorApplication.java
+++ src/main/java/com/htecgroup/flightadvisor/FlightAdvisorApplication.java
@@ -3,12 +3,11 @@ package com.htecgroup.flightadvisor;
 import com.htecgroup.flightadvisor.config.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 @SpringBootApplication
-@EnableConfigurationProperties({LiquibaseProperties.class, ApplicationProperties.class})
+@EnableConfigurationProperties({ApplicationProperties.class})
 public class FlightAdvisorApplication {
 
     public static void main(String[] args) {
